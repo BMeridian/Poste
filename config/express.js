ea637ce0a5831c57ff3952bface99a928344c4e3
Chris Grant <chris@chrisgrants.com>
2014-05-24 17:56:20 -0400

    sorry

---
+++ config/express.js
@@ -0,0 +1,83 @@
+var passport            = require('passport')
+    , GitHubStrategy    = require('passport-github').Strategy
+    , FacebookStrategy  = require('passport-facebook').Strategy
+    , GoogleStrategy    = require('passport-google-oauth').OAuth2Strategy
+    , TwitterStrategy   = require('passport-twitter').Strategy
+    , LocalStrategy     = require();
+
+
+var verifyHandler = function(token, tokenSecret, profile, done) {
+  process.nextTick(function() {
+
+    User.findOne({id: profile.id}, function(err, user) {
+      if (user) {
+        return done(null, user);
+      } else {
+
+        var data = {
+          provider: profile.provider,
+          uid: profile.id,
+          name: profile.displayName
+        };
+
+        if (profile.emails && profile.emails[0] && profile.emails[0].value) {
+          data.email = profile.emails[0].value;
+        }
+        if (profile.name && profile.name.givenName) {
+          data.fistname = profile.name.givenName;
+        }
+        if (profile.name && profile.name.familyName) {
+          data.lastname = profile.name.familyName;
+        }
+
+        User.create(data, function(err, user) {
+          return done(err, user);
+        });
+      }
+    });
+  });
+};
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findOne({id: id}, function(err, user) {
+    done(err, user)
+  });
+});
+
+
+module.exports.express = {
+
+  customMiddleware: function(app) {
+
+    passport.use(new GitHubStrategy({
+      clientID: "YOUR_CLIENT_ID",
+      clientSecret: "YOUR_CLIENT_SECRET",
+      callbackURL: "http://localhost:1337/auth/github/callback"
+    }, verifyHandler));
+
+    passport.use(new FacebookStrategy({
+      clientID: "YOUR_CLIENT_ID",
+      clientSecret: "YOUR_CLIENT_SECRET",
+      callbackURL: "http://localhost:1337/auth/facebook/callback"
+    }, verifyHandler));
+
+    passport.use(new GoogleStrategy({
+      clientID: 'YOUR_CLIENT_ID',
+      clientSecret: 'YOUR_CLIENT_SECRET',
+      callbackURL: 'http://localhost:1337/auth/google/callback'
+    }, verifyHandler));
+
+    passport.use(new TwitterStrategy({
+      consumerKey: 'YOUR_CLIENT_ID',
+      consumerSecret: 'YOUR_CLIENT_SECRET',
+      callbackURL: 'http://localhost:1337/auth/twitter/callback'
+    }, verifyHandler));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
+};
