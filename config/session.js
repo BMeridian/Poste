ec7de9c8e24a20f4874e90c2b0dcd69b574546a6
Chris Grant <chris@chrisgrants.com>
2014-05-24 17:34:38 -0400

    v10

--- config/session.js
+++ config/session.js
@@ -1,13 +1,15 @@
 /**
- * Session
+ * Session Configuration
+ * (sails.config.session)
  *
- * Sails session integration leans heavily on the great work already done by Express, but also unifies
- * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
- * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
- * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
+ * Sails session integration leans heavily on the great work already done by
+ * Express, but also unifies Socket.io with the Connect session store. It uses
+ * Connect's cookie parser to normalize configuration differences between Express
+ * and Socket.io and hooks into Sails' middleware interpreter to allow you to access
+ * and auto-save to `req.session` with Socket.io the same way you would with Express.
  *
  * For more information on configuring the session, check out:
- * http://sailsjs.org/#documentation
+ * http://links.sailsjs.org/docs/config/session
  */
 
 module.exports.session = {
@@ -15,17 +17,33 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again.
-  secret: '3316b4b9e4616b6aa8291db46bb2f3c0'//,
+  secret: 'f49b323fc3a213fba31451b2e8a4204a',
+
+
+  // Set the session cookie expire time
+  // The maxAge is set by milliseconds, the example below is for 24 hours
+  //
+  // cookie: {
+  //   maxAge: 24 * 60 * 60 * 1000
+  // }
 
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  //adapter: 'redis',
+  // adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
   // Read more about options at: https://github.com/visionmedia/connect-redis
   //
+  // host: 'localhost',
+  // port: 6379,
+  // ttl: <redis session TTL in seconds>,
+  // db: 0,
+  // pass: <redis auth password>
+  // prefix: 'sess:'
+
+  //OLD STUFFz
   //host: 'redis://redistogo:3316b4b9e4616b6aa8291db46bb2f3c0@angelfish.redistogo.com',
   //port: 10050,
   //ttl: 2000,
